b605121e1abe0ee7cf1ec5583bdbbd6e2267e87e
phonginreallife <112053396+phonginreallife@users.noreply.github.com>
2023-03-08 11:55:43 +0700

    updates change

--- script.js
+++ script.js
@@ -1,65 +1,110 @@
-// Add your code here to retrieve values from the sensors and update the interface
-const page1Button = document.getElementById("living-room-btn");
-const page2Button = document.getElementById("kitchen-btn");
-const page3Button = document.getElementById("bedroom-btn");
-const page1Container = document.getElementById("livingroom");
-const page2Container = document.getElementById("kitchen");
-const page3Container = document.getElementById("bedroom");
+document.addEventListener("DOMContentLoaded", () => {
+  // Add your code here to retrieve values from the sensors and update the interface
+  const page1Button = document.getElementById("living-room-btn");
+  const page2Button = document.getElementById("kitchen-btn");
+  const page3Button = document.getElementById("bedroom-btn");
+  const page1Container = document.getElementById("livingroom");
+  const page2Container = document.getElementById("kitchen");
+  const page3Container = document.getElementById("bedroom");
+  
+  page1Button.addEventListener("click", () => {
+    page1Container.classList.add("show");
+    page2Container.classList.remove("show");
+    page3Container.classList.remove("show");
+  });
+  
+  page2Button.addEventListener("click", () => {
+    page1Container.classList.remove("show");
+    page2Container.classList.add("show");
+    page3Container.classList.remove("show");
+  });
+  
+  page3Button.addEventListener("click", () => {
+    page1Container.classList.remove("show");
+    page2Container.classList.remove("show");
+    page3Container.classList.add("show");
+  });
+  });
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyAMuREfO7bBb5_eLpFYnCSS71Qur7RHezg",
+	authDomain: "smarthome-interface.firebaseapp.com",
+	databaseURL: "https://smarthome-interface-default-rtdb.asia-southeast1.firebasedatabase.app",
+	projectId: "smarthome-interface",
+	storageBucket: "smarthome-interface.appspot.com",
+	messagingSenderId: "87457114680",
+	appId: "1:87457114680:web:8123ba2fba8905e642d4cd",
+	measurementId: "G-4BECG4N2W7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-page1Button.addEventListener("click", () => {
-  page1Container.classList.add("show");
-  page2Container.classList.remove("show");
-  page3Container.classList.remove("show");
+
+// Auto load Temperature-------------------------
+firebase.database().ref("/TT_IoT/temperature").on("value",function(snapshot){
+  var nd = snapshot.val();  
+  document.getElementById("temp").innerHTML = nd;
+  console.log(nd);
+});
+firebase.database().ref("/TT_IoT/huminity").on("value",function(snapshot){
+  var nd = snapshot.val();  
+  document.getElementById("hum").innerHTML = nd;
+  console.log(nd);
 });
 
-page2Button.addEventListener("click", () => {
-  page1Container.classList.remove("show");
-  page2Container.classList.add("show");
-  page3Container.classList.remove("show");
+
+
+	$(document).ready(function(){
+    var database = firebase.database();
+	  var Led1Status;
+
+	database.ref("/TT_IoT/Led1Status").on("value", function(snapshot){
+		Led1Status = snapshot.val();
+		if(Led1Status == "1"){    // check from the firebase
+			//$(".Light1Status").text("The light is off");
+			document.getElementById("unact").style.display = "none";
+			document.getElementById("act").style.display = "block";
+      document.getElementById("d01_img").src = "./image/lamp-on.png"
+		} else {
+			//$(".Light1Status").text("The light is on");
+			document.getElementById("unact").style.display = "block";
+			document.getElementById("act").style.display = "none";
+      document.getElementById("d01_img").src = "./image/lamp-off.png"
+		}
+	});
+
+    $(".toggle-btn").click(function(){
+		var firebaseRef = firebase.database().ref("TT_IoT/").child("Led1Status");
+
+		if(Led1Status == "1"){    // post to firebase
+			firebaseRef.set("0");
+			Led1Status = "0";
+		} else {
+			firebaseRef.set("1");
+			Led1Status = "1";
+		}
+	})
 });
 
-page3Button.addEventListener("click", () => {
-  page1Container.classList.remove("show");
-  page2Container.classList.remove("show");
-  page3Container.classList.add("show");
+$(document).ready(function(){
+  var database = firebase.database();
+  var airCondision;
+  database.ref("/TT_IoT/air-condition").on("value", function(snapshot){
+  airCondision = snapshot.val();
+  });
+  $(".toggle").click(function(){
+            var firebaseRef = firebase.database().ref("TT_IoT/").child("air-condition");
+              if( airCondision == "1"){    
+                    firebaseRef.set("0");
+                    airCondision = "0";
+              } else {
+                    firebaseRef.set("1");
+                    airCondision = "1";
+              }
+}); 
 });
-// // Example values for the sensors
-// var temperature = 25;
-// var humidity = 50;
-// var gas = 100;
-// var light = true;
 
-// // Example values for the devices
-// var devices = [
-//   { name: 'Device 1', value: 50 },
-//   { name: 'Device 2', value: 75 },
-//   { name: 'Device 3', value: 25 },
-//   { name: 'Device 4', value: 0 },
-//   { name: 'Device 5', value: 100 },
-//   { name: 'Device 6', value: 50 }
-// ];
+// $(".circle").onmouseover(function(){
 
 
-// // Function to update the sensor values in the interface
-// function updateSensors() {
-//   document.getElementById('temperature-value').innerHTML = temperature + '°C';
-//   document.getElementById('humidity-value').innerHTML = humidity + '%';
-//   document.getElementById('gas-value').innerHTML = gas + ' ppm';
-//   document.getElementById('light-value').innerHTML = light ? 'On' : 'Off';
-// }
-
-// // Function to update the device values in the interface
-// function updateDevices() {
-//   for (var i = 0; i < devices.length; i++) {
-//     var device = devices[i];
-//     var slider = document.getElementById('device-' + (i + 1) + '-slider');
-//     slider.value = device.value;
-//     document.getElementById('device-' + (i + 1) + '-value').innerHTML = device.value + '%';
-//   }
-// }
-
-// // Call the update functions when the page loads
-// window.onload = function() {
-//   updateSensors();
-//   updateDevices();
-// };
+// })
